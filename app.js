eba4620741145609d5aa92e9028f856e16d43d44
flo <xxflo.h13@gmail.com>
2018-04-24 20:21:35 -0700

    fix price

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://xxflo:ginflo@ds257589.mlab.com:57589/yelp-camp--clone");
 
-// console.log(process.env.DATABASEURL);
+var url = process.env.DATABASEURL||"mongodb://localhost/yelp_camp";
+mongoose.connect(url);
+// mongoose.connect("mongodb://xxflo:ginflo@ds257589.mlab.com:57589/yelp-camp--clone");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(flash());
